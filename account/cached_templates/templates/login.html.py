ebced7dc3baa45d477ca0173600f326e32c6cce4
jordancarlson08 <jordancarlson08@gmail.com>
2014-04-10 20:18:34 -0600

    login error beautiful

--- account/cached_templates/templates/login.html.py
+++ account/cached_templates/templates/login.html.py
@@ -4,13 +4,13 @@ UNDEFINED = runtime.UNDEFINED
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 9
-_modified_time = 1397180826.36789
+_modified_time = 1397182672.485292
 _enable_loop = True
-_template_filename = '/Users/ecookson/Desktop/MyStuff/account/templates/login.html'
+_template_filename = 'C:\\Users\\Jordan Carlson\\Desktop\\MyStuff\\account\\templates/login.html'
 _template_uri = 'login.html'
 _source_encoding = 'ascii'
 import os, os.path, re
-_exports = ['top', 'content']
+_exports = ['content', 'top']
 
 
 def _mako_get_namespace(context, name):
@@ -28,26 +28,86 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        form = context.get('form', UNDEFINED)
-        def top():
-            return render_top(context._locals(__M_locals))
         def content():
             return render_content(context._locals(__M_locals))
+        def top():
+            return render_top(context._locals(__M_locals))
+        form = context.get('form', UNDEFINED)
         __M_writer = context.writer()
         # SOURCE LINE 2
-        __M_writer('\n')
+        __M_writer('\r\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
             context['self'].content(**pageargs)
         
 
-        # SOURCE LINE 80
-        __M_writer('  \n\n')
+        # SOURCE LINE 83
+        __M_writer('  \r\n\r\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'top'):
             context['self'].top(**pageargs)
         
 
-        # SOURCE LINE 82
-        __M_writer('\n\n\n')
+        # SOURCE LINE 85
+        __M_writer('\r\n\r\n\r\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_content(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def content():
+            return render_content(context)
+        form = context.get('form', UNDEFINED)
+        __M_writer = context.writer()
+        # SOURCE LINE 3
+        __M_writer('\r\n')
+        # SOURCE LINE 4
+ 
+
+        __M_writer('\r\n \r\n\r\n<br/>\r\n<div class="container-fluid">\r\n\r\n\r\n<form class ="form-horizontal" role="form" method ="POST" action=\'/account/login/\'>\r\n\r\n')
+        # SOURCE LINE 13
+        for f in form:
+            # SOURCE LINE 14
+            if f.name!='remember':
+                # SOURCE LINE 15
+                __M_writer('  <div class="row">\r\n    <div class="form-group">\r\n      <label class="col-md-3 control-label" for="id_')
+                # SOURCE LINE 17
+                __M_writer(str( f.name ))
+                __M_writer('">')
+                __M_writer(str( f.label ))
+                __M_writer('</label>\r\n\r\n        <div class="col-md-7">\r\n          ')
+                # SOURCE LINE 20
+                __M_writer(str(f))
+                __M_writer(' \r\n          \r\n          ')
+                # SOURCE LINE 22
+                __M_writer(str(f.errors))
+                __M_writer(' \r\n        </div>\r\n    </div>\r\n  </div>\r\n')
+        # SOURCE LINE 28
+        __M_writer('\r\n  <div class="form-group">\r\n    <label for="inputPassword3" class="col-md-3 control-label"></label>\r\n    <div class="col-md-4">\r\n      <a href="/account/forgotpassword/">Forgot My Password?</a>\r\n    </div>\r\n  </div>\r\n\r\n')
+        # SOURCE LINE 36
+        for f in form:
+            # SOURCE LINE 37
+            if f.name=='remember':
+                # SOURCE LINE 38
+                __M_writer('    <div class="form-group">\r\n      <div class="col-md-offset-3 col-md-4">\r\n        <div class="checkbox">\r\n          <label>\r\n        ')
+                # SOURCE LINE 42
+                __M_writer(str(f))
+                __M_writer(' \r\n          ')
+                # SOURCE LINE 43
+                __M_writer(str(f.errors))
+                __M_writer(' Remember me\r\n          </label>\r\n         </div>\r\n      </div>\r\n    </div>\r\n')
+        # SOURCE LINE 50
+        __M_writer("\r\n<br/>\r\n\r\n<div class='row'>\r\n  <div class='col-md-3'>\r\n  </div>\r\n")
+        # SOURCE LINE 56
+        if form.non_field_errors():
+            # SOURCE LINE 57
+            __M_writer('  <div class=\'col-md-7\'>\r\n     <div class="alert alert-danger">\r\n       ')
+            # SOURCE LINE 59
+            __M_writer(str(form.non_field_errors()))
+            __M_writer('\r\n      </div>\r\n  </div>\r\n')
+        # SOURCE LINE 63
+        __M_writer('</div>\r\n\r\n  <div class="form-group">\r\n    <div class="col-md-offset-3 col-md-8">\r\n\r\n      <ul class=\'list-inline\'>\r\n        <li><button id=\'submit_button\' class="btn btn-primary" type="submit">Sign In</button></li>\r\n        <li><a href="/account/newuser/" class="btn btn-success">Create Account</a></li>\r\n      </ul>\r\n\r\n    </div>\r\n  </div> \r\n</form>\r\n  \r\n  \r\n\r\n\r\n\r\n\r\n\r\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -59,66 +119,6 @@ def render_top(context,**pageargs):
         def top():
             return render_top(context)
         __M_writer = context.writer()
-        return ''
-    finally:
-        context.caller_stack._pop_frame()
-
-
-def render_content(context,**pageargs):
-    __M_caller = context.caller_stack._push_frame()
-    try:
-        form = context.get('form', UNDEFINED)
-        def content():
-            return render_content(context)
-        __M_writer = context.writer()
-        # SOURCE LINE 3
-        __M_writer('\n')
-        # SOURCE LINE 4
- 
-
-        __M_writer('\n \n\n<br/>\n<div class="container-fluid">\n\n\n<form class ="form-horizontal" role="form" method ="POST" action=\'/account/login/\'>\n\n')
-        # SOURCE LINE 13
-        for f in form:
-            # SOURCE LINE 14
-            if f.name!='remember':
-                # SOURCE LINE 15
-                __M_writer('  <div class="row">\n    <div class="form-group">\n      <label class="col-md-3 control-label" for="id_')
-                # SOURCE LINE 17
-                __M_writer(str( f.name ))
-                __M_writer('">')
-                __M_writer(str( f.label ))
-                __M_writer('</label>\n\n        <div class="col-md-7">\n          ')
-                # SOURCE LINE 20
-                __M_writer(str(f))
-                __M_writer(' \n          \n          ')
-                # SOURCE LINE 22
-                __M_writer(str(f.errors))
-                __M_writer(' \n        </div>\n    </div>\n  </div>\n')
-        # SOURCE LINE 28
-        __M_writer('\n  <div class="form-group">\n    <label for="inputPassword3" class="col-md-3 control-label"></label>\n    <div class="col-md-4">\n      <a href="/account/forgotpassword/">Forgot My Password?</a>\n    </div>\n  </div>\n\n')
-        # SOURCE LINE 36
-        for f in form:
-            # SOURCE LINE 37
-            if f.name=='remember':
-                # SOURCE LINE 38
-                __M_writer('    <div class="form-group">\n      <div class="col-md-offset-3 col-md-4">\n        <div class="checkbox">\n          <label>\n        ')
-                # SOURCE LINE 42
-                __M_writer(str(f))
-                __M_writer(' \n          ')
-                # SOURCE LINE 43
-                __M_writer(str(f.errors))
-                __M_writer(' Remember me\n          </label>\n         </div>\n      </div>\n    </div>\n')
-        # SOURCE LINE 50
-        __M_writer('\n<br/>\n  <div class="form-group">\n    <div class="col-md-offset-3 col-md-8">\n\n      <ul class=\'list-inline\'>\n        <li><button id=\'submit_button\' class="btn btn-primary" type="submit">Sign In</button></li>\n        <li><a href="/account/newuser/" class="btn btn-success">Create Account</a></li>\n      </ul>\n\n    </div>\n  </div> \n</form>\n  \n  \n  <div class=\'row\'>\n    <div class=\'col-md-3\'>\n    </div>\n')
-        # SOURCE LINE 68
-        if form.non_field_errors:
-            # SOURCE LINE 69
-            __M_writer('    <div class=\'col-md-5\'>\n       <div class="alert alert-danger">\n         ')
-            # SOURCE LINE 71
-            __M_writer(str(form.non_field_errors()))
-            __M_writer('\n        </div>\n    </div>\n')
-        # SOURCE LINE 75
-        __M_writer('  </div>\n\n\n\n\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
