4f4f6fdd298ce00e4a1f8a4621aaf94c0ccdb773
jordancarlson08 <jordancarlson08@gmail.com>
2014-11-04 14:25:22 -0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,23 +132,30 @@ INSTALLED_APPS = (
 
 # django.db.backends.postgresql_psycopg2
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'C:\Python33\db.sqlite3'
-#     }
+# DATABASES = 
+# 'default': {
+#      'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': 'C:\Python33\db.sqlite3'
+#    }
 # }
-
+ 
 DATABASES = {
     'default': {
-        'ENGINE':'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mystuff',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE':'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'mystuff',
+#        'USER': 'postgres',
+#        'PASSWORD': 'admin',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+#}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
